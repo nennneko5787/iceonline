f97dc0de3912e3443bfb0ecd88456e3029b958ad
ねんねこ <nennneko5787@gmail.com>
2024-11-28 23:43:21 +0900

    userinfo.py を更新

--- cogs/userinfo.py
+++ cogs/userinfo.py
@@ -92,7 +92,7 @@ class UserInfoCog(commands.Cog):
         )
 
         if not userData.get("birthday_secret", False):
-            birthDayDateTime = datetime.strptime(userData.get("birthday", "1/1/1970 00:00:00 AM"), "%m/%d/%Y %I:%M:%S %p").replace(tzinfo=ZoneInfo("Asia/Tokyo")
+            birthDayDateTime = datetime.strptime(userData.get("birthday", "1/1/1970 00:00:00 AM"), "%m/%d/%Y %I:%M:%S %p").replace(tzinfo=ZoneInfo("Asia/Tokyo"))
             
             embed.add_field(
                 name=await self.bot.tree.translator.translate(
