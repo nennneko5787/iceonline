ac723f61252e01bf830e3848bfc1b13515d6b9ae
ねんねこ <nennneko5787@gmail.com>
2024-12-03 21:56:50 +0900

    userinfo.py を更新

--- cogs/userinfo.py
+++ cogs/userinfo.py
@@ -178,18 +178,17 @@ class UserInfoCog(commands.Cog):
             interaction.locale,
         )
 
-        if not userData.get("birthday_secret", False):
-            birthDayDateTime = datetime.strptime(
-                userData.get("birthday", "1/1/1970 00:00:00 AM"), "%m/%d/%Y %I:%M:%S %p"
-            ).replace(tzinfo=ZoneInfo("Asia/Seoul"))
+        birthDayDateTime = datetime.strptime(
+            userData.get("birthday", "1/1/1970 00:00:00 AM"), "%m/%d/%Y %I:%M:%S %p"
+        ).replace(tzinfo=ZoneInfo("Asia/Seoul"))
 
-            profileEmbed.add_field(
-                name=await self.bot.tree.translator.translate(
-                    app_commands.locale_str("誕生日"),
-                    interaction.locale,
-                ),
-                value=discord.utils.format_dt(birthDayDateTime),
-            )
+        profileEmbed.add_field(
+            name=await self.bot.tree.translator.translate(
+                app_commands.locale_str("誕生日"),
+                interaction.locale,
+            ),
+            value=discord.utils.format_dt(birthDayDateTime),
+        )
 
         for i in range(1, 9):
             if userData.get(f"info_data_{i}", "") == "":
